0bb9a2867dd55da93bbf7cf4a99fb18a45beeff0
16104013 <16104013@facin.portoalegre.pucrsnet.br>
2019-09-05 21:06:45 -0300

    strcpy

---
+++ software/tests/libc/strcpy/test.c
@@ -0,0 +1,64 @@
+//Eduardo Porto e Johannes Portz
+//T1 Teste e Confiabilidade de Sistemas
+#include <hf-risc.h>
+#include <hf-unit.h>
+
+// function under test
+int8_t *strcpy(int8_t *dst, const int8_t *src);
+
+// list of individual tests
+void test1();
+void test2();
+void test3();
+void test4();
+void test5();
+
+
+// main test
+void hfunit_run_tests_strcpy(){
+	test1();
+	test2();
+	test3();
+	test4();
+	test5();
+
+}
+
+// teste simples copiando a string "teste 1" a partir de uma variável para uma variavel destino do tamanho exato
+void test1(){
+	char srcString[] = "teste 1";
+	char dstString[7];
+	strcpy(dstString, srcString);
+	hfunit_comp_vector(dstString, srcString, 7, "");
+}
+
+// teste extra semelhante ao anterior, passando uma string diretamente para a funcao
+void test2(){
+	char dstString[7];
+	strcpy(dstString, "teste 2");
+	hfunit_comp_vector(dstString, "teste 2", 7, "");
+}
+
+//teste passando uma string de tamanho 7 para uma variavel de tamanho 5
+void test3(){
+	char srcString[] = "teste 1";
+	char dstString[5];
+	strcpy(dstString, srcString);
+	hfunit_comp_vector(dstString, srcString, 5, "");
+}
+
+//testando passando um char sem ser vetor, passando o endereco desse char para a funcao
+void test4(){
+	char srcString = ' ';
+	char dstString[1];
+	strcpy(dstString, &srcString);
+	hfunit_comp_vector(dstString, &srcString, 1, "");
+}
+
+//teste passando o endereco de um inteiro para um array de char de tamanho 4 para testar passando diferentes tipos de variáveis
+void test5(){
+	int src = 1234;
+	char dstString[4];
+	strcpy(dstString, &src);
+	hfunit_comp_vector(dstString, &src, 4, "");
+}
