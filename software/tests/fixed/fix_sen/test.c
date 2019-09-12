598ff4866dda4c8cb829173a7708026d6d325c07
portoedu <eduardo.porto@acad.pucrs.br>
2019-09-12 00:06:16 -0300

    wip

--- software/tests/fixed/fix_sen/test.c
+++ software/tests/fixed/fix_sen/test.c
@@ -31,75 +31,102 @@ void hfunit_run_tests_fix_sin(){
 	test9();
 }
 
-//
+//Teste com o valor de PI, que é um dos valores de entrada que são testados
 void test1(){
-	fixed_t expected = 0;
+	fixed_t expected = fix_val(0);
 	fixed_t received = fix_sin(-FIX_PI);
 	hfunit_comp_vector(&received, &expected, 8, "");
-	printf("ex: %ld - rc: %ld \n\n", expected, received);
+	printf("ex: %d - rc: %d \n", fix_int(expected), fix_int(received));
 	printBits(8, &expected);
 	printBits(8, &received);
+	printf("\n\n");
 }
 
-//
+//Teste semelhante ao anterior, dessa vez com o valor positivo, para observarmos se há diferença
 void test2(){
-	fixed_t expected = 0;
+	fixed_t expected = fix_val(0);
 	fixed_t received = fix_sin(FIX_PI);
 	hfunit_comp_vector(&received, &expected, 8, "");
-	printf("ex: %d - rc: %d \n\n", expected, received);
+	printf("ex: %d - rc: %d \n", fix_int(expected), fix_int(received));
+	printBits(8, &expected);
+	printBits(8, &received);
+	printf("\n\n");
 }
 
-//
+//Teste com o valor 0 pois é a metade entre os valores utilizados na função, -pi a pi
 void test3(){
-	fixed_t expected = 0;
+	fixed_t expected = fix_val(0);
 	fixed_t received = fix_sin(0);
 	hfunit_comp_vector(&received, &expected, 8, "");
-	printf("ex: %d - rc: %d \n\n", expected, received);
+	printf("ex: %d - rc: %d \n", fix_int(expected), fix_int(received));
+	printBits(8, &expected);
+	printBits(8, &received);
+	printf("\n\n");
 }
 
-//
+//Teste com um valor bem pŕoximo de 0.
 void test4(){
-	fixed_t expected = 0;
+	fixed_t expected = fix_val(0);
 	fixed_t received = fix_sin(0.0000001);
 	hfunit_comp_vector(&received, &expected, 8, "");
-	printf("ex: %d - rc: %d \n\n", expected, received);
+	printf("ex: %d - rc: %d \n", fix_int(expected), fix_int(received));
+	printBits(8, &expected);
+	printBits(8, &received);
+	printf("\n\n");
 }
 
 //
+//Teste com valor que se encontra na metade entre 0 e pi, que são os valores positivos aceitos
 void test5(){
-	fixed_t expected = 1;
+	fixed_t expected = fix_val(1);
 	fixed_t received = fix_sin(FIX_HALF_PI);
 	hfunit_comp_vector(&received, &expected, 8, "");
-	printf("ex: %d - rc: %d \n\n", expected, received);
+	printf("ex: %d - rc: %d \n", fix_int(expected), fix_int(received));
+	printBits(8, &expected);
+	printBits(8, &received);
+	printf("\n\n");
 }
 
-//
+//Teste extra, com valor de 1/4 de pi para vermos a precisão do resultado, no resultado esperado a precisão é de 3 casas depois da virgula
 void test6(){
-	fixed_t expected = 1;
+	fixed_t expected = fix_val(0.707);
 	fixed_t received = fix_sin(FIX_FOURTH_PI);
 	hfunit_comp_vector(&received, &expected, 8, "");
-	printf("ex: %d - rc: %d \n\n", expected, received);
+	printf("ex: %d - rc: %d \n", fix_int(expected), fix_int(received));
+	printBits(8, &expected);
+	printBits(8, &received);
+	printf("\n\n");
 }
 
-//
+//Teste com o valor maior que o utilziado na função, sendo dentro dela diminuido 2pi
 void test7(){
-	fixed_t expected = 0;
+	fixed_t expected = fix_val(0);
 	fixed_t received = fix_sin(FIX_TWO_PI);
 	hfunit_comp_vector(&received, &expected, 8, "");
-	printf("ex: %d - rc: %d \n\n", expected, received);
+	printf("ex: %d - rc: %d \n", fix_int(expected), fix_int(received));
+	printBits(8, &expected);
+	printBits(8, &received);
+	printf("\n\n");
 }
 
-//
+//Teste extra com um valor diferente de algum referencial de pi
 void test8(){
-	fixed_t expected = 1;
-	fixed_t received = fix_sin(2);
+	fixed_t expected = fix_val(0.909);
+	fixed_t received = fix_sin(fix_val(2));
 	hfunit_comp_vector(&received, &expected, 8, "");
-	printf("ex: %d - rc: %d \n\n", expected, received);
+	printf("ex: %d - rc: %d \n", fix_int(expected), fix_int(received));
+	printBits(8, &expected);
+	printBits(8, &received);
+	printf("\n\n");
 }
 
+//Teste extra apenas para observar o resultado negativo desta função
 void test9(){
-	fixed_t expected = -1;
+	fixed_t expected = fix_val(-1);
 	fixed_t received = fix_sin(3*FIX_HALF_PI);
 	hfunit_comp_vector(&received, &expected, 8, "");
-	printf("ex: %d - rc: %d \n\n", expected, received);
+	printf("ex: %d - rc: %d \n", fix_int(expected), fix_int(received));
+	printBits(8, &expected);
+	printBits(8, &received);
+	printf("\n\n");
 }
