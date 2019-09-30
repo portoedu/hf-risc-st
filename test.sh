6cbdc8507334074a6618f7065d6d9298340b2eac
portoedu <eduardo.porto@acad.pucrs.br>
2019-09-29 22:18:04 -0300

    wip

--- test.sh
+++ test.sh
@@ -3,30 +3,42 @@
 cd ./software/tests/math/ceil/
 cppcheck test.c
 make test
+gcov hf_riscv_sim.c
+
 cd ../floor/
 cppcheck test.c
 make test
+gcov hf_riscv_sim.c
 
 cd ../..
 cd ./libc/strcpy/
 cppcheck test.c
 make test
+gcov hf_riscv_sim.c
+
 cd ../strlen/
 cppcheck test.c
 make test
+gcov hf_riscv_sim.c
 
 cd ../..
 cd ./fixed/fix_cos/
 cppcheck test.c
 make test
+gcov hf_riscv_sim.c
+
 cd ../fix_sen/
 cppcheck test.c
 make test
+gcov hf_riscv_sim.c
 
 cd ../..
 cd ./matrix/get_line/
 cppcheck test.c
 make test
+gcov hf_riscv_sim.c
+
 cd ../get_column/
 cppcheck test.c
 make test
+gcov hf_riscv_sim.c
